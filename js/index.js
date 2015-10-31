ecb0b2799286539f7254b22427f329e6799ed6bb
ecthurlow <ecthurlow@rogers.com>
2015-10-31 15:21:33 -0400

    Website!

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/*-----------------------------------------------------------------------
+=PARALAX city
+-----------------------------------------------------------------------*/
+/*$(document).ready(function(){
+  $(document).mousemove(function(e){
+    var xMouse = e.pageX;
+    var xCenter = $(window).width()/2;
+    var offset = xMouse - xCenter;
+    $(".forest").css({"left": -offset*0.03});
+    $(".city1").css({"left": -offset*0.02});
+    $(".city2").css({"left": -offset*0.01});
+   });//end mouse move
+});*/
