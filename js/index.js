9cf6ca5cb3f846dd48f5d65273901c4dd7e72ccd
ecthurlow <ecthurlow@rogers.com>
2015-10-31 16:55:09 -0400

    Parallax and Spinning logos

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
 /*-----------------------------------------------------------------------
 =PARALAX city
 -----------------------------------------------------------------------*/
-/*$(document).ready(function(){
+$(document).ready(function(){
   $(document).mousemove(function(e){
     var xMouse = e.pageX;
     var xCenter = $(window).width()/2;
     var offset = xMouse - xCenter;
-    $(".forest").css({"left": -offset*0.03});
-    $(".city1").css({"left": -offset*0.02});
-    $(".city2").css({"left": -offset*0.01});
-   });//end mouse move
-});*/
+    $("#parallax").css({"background-position-x": -offset*0.03 + "px," +  -offset*0.02 + "px," + -offset*0.01 + "px" });//foreground
+   });
+});
